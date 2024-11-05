8d9dc6ed3a492cecc2edac924d1d07b97548a96c
Aswathi-P-V <aswathipv084@gmail.com>
2024-11-05 15:09:44 +0530

    profile page updated

--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   username: { type: String, required: true, unique: true },
-  mobile: { type: String },
+  phone: { type: String },
   address: { type: String },
   creditCard: { type: String },
 });
